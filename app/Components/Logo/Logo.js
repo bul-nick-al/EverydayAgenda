dbc74d683e34bbdcea970075f075fc4b6733b91d
Nastya <anastasia.vorton@gmail.com>
2018-04-12 08:41:42 +0300

    Logo added + global font updated

---
+++ app/Components/Logo/Logo.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Nastya on 12.04.2018.
+ */
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+const Logo = () => (
+    <View style={styles.container}>
+        <Text style={styles.text}>EVERYDAY{"\n"}AGENDA</Text>
+    </View>
+);
+
+export default Logo;
